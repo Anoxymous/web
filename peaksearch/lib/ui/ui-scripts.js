9ab61f3f7735e0fe671d65fe832c68cabfdae065
Anoxymous <timy.cross@gmail.com>
2025-05-18 20:46:58 +1000

    added location lookup

--- peaksearch/lib/ui/ui-scripts.js
+++ peaksearch/lib/ui/ui-scripts.js
@@ -1,9 +1,3 @@
-window.addEventListener("load", function() { ui_onLoad() } );
-
-function ui_onLoad()
-{
-	setupAutocomplete("location");
-}
 
 // TODO make an array based on id
 let ui_controller;
@@ -44,7 +38,12 @@ async function autocompleteLocation(input, suggestionBox)
 	}
 	else
 	{
+		// Re-position and display
+		input.parentElement.appendChild(suggestionBox);
+		var y_pos = input.offsetTop + input.getBoundingClientRect().height
+		suggestionBox.style.top = y_pos +'px';
 		suggestionBox.style.display = "block";
+
 		suggestionBox.innerHTML = ""; // clear any previous
 		var loading_div = document.createElement("div");
 		loading_div.className = "ui_suggestion-loading";
@@ -85,17 +84,14 @@ async function autocompleteLocation(input, suggestionBox)
 	}
 }
 
-function setupAutocomplete(id)
+function setupAutocomplete(input)
 {
-	var input = document.getElementById(id);
 	if(input != null)
 	{
+		var id = input.getAttribute("id")
 		const suggestionBox = document.createElement("div");
 		suggestionBox.setAttribute("id",  id + "_ui_suggestions");
 		suggestionBox.className = "ui_suggestions";
-		input.parentElement.appendChild(suggestionBox);
-		var y_pos = input.offsetTop + input.getBoundingClientRect().height
-		suggestionBox.style.top = y_pos +'px';
 
 		input.addEventListener("input", async () => { autocompleteLocation(input, suggestionBox) } );
 
@@ -105,5 +101,95 @@ function setupAutocomplete(id)
 				suggestionBox.style.display = "none";
 				}
 		});
+	}
+}
+
+function updateIndicators() {
+						
+            const lat = parseFloat(document.getElementById("latitude").value) || 0;
+            const lon = parseFloat(document.getElementById("longitude").value) || 0;
+
+            document.getElementById("lat-indicator").innerText = lat >= 0 ? "N" : "S";
+            document.getElementById("lon-indicator").innerText = lon >= 0 ? "E" : "W";
+        }
+
+function createLocationLookupForm(parentId, id, title, callback)
+{
+	var parentDiv = document.getElementById(parentId);
+	if(parentDiv != null)
+	{
+		var new_div = document.createElement("div");
+		new_div.setAttribute("id",  id);
+		new_div.className = "ui_form";
+		parentDiv.appendChild(new_div);
+
+		var eTitle = document.createElement("h2");
+		eTitle.innerHTML = title;
+		new_div.appendChild(eTitle);
+		
+		var eForm = document.createElement("form");
+		eForm.addEventListener("submit", (event) => {
+			event.preventDefault(); // Prevent actual form submission
+			var formElements = event.target.elements;
+			var loc = formElements["location"].value;
+			var lat = formElements["latitude"].value;
+			var lon = formElements["longitude"].value;
+			callback(lat, lon, loc); // Pass data to the callback function
+		});
+		new_div.appendChild(eForm);
+
+		var eInputLoc = document.createElement("input");
+		eInputLoc.type = "text";
+		eInputLoc.setAttribute("id", "location");
+		eInputLoc.setAttribute("placeholder", "Enter Name...");
+		eForm.appendChild(eInputLoc);
+		
+		var eDivLat = document.createElement("div");
+		eDivLat.className = "input-group";
+		eForm.appendChild(eDivLat);
+
+		var eInputLat = document.createElement("input");
+		eInputLat.type = "number";
+		eInputLat.setAttribute("id", "latitude");
+		eInputLat.setAttribute("placeholder", "Latitude (-90 to 90)");
+		eInputLat.setAttribute("min", "-90");
+		eInputLat.setAttribute("max", "90");
+		eInputLat.setAttribute("step", "0.0000001");
+		eInputLat.addEventListener("oninput", (event) => { updateIndicators() } );
+		eDivLat.appendChild(eInputLat);
+
+		var eSpanLat = document.createElement("span");
+		eSpanLat.className = "indicator";
+		eSpanLat.setAttribute("id", "lat-indicator");
+		eSpanLat.innerText = "N/S"
+		eDivLat.appendChild(eSpanLat);
+
+		var eDivLon = document.createElement("div");
+		eDivLon.className = "input-group";
+		eForm.appendChild(eDivLon);
+		
+		var eInputLon = document.createElement("input");
+		eInputLon.type = "number";
+		eInputLon.setAttribute("id", "longitude");
+		eInputLon.setAttribute("placeholder", "Longitude (-180 to 180)");
+		eInputLon.setAttribute("min", "-180");
+		eInputLon.setAttribute("max", "180");
+		eInputLon.setAttribute("step", "0.0000001");
+		eInputLon.addEventListener("oninput", (event) => { updateIndicators() } );
+		eDivLon.appendChild(eInputLon);
+		
+		var eSpanLon = document.createElement("span");
+		eSpanLon.className = "indicator";
+		eSpanLon.setAttribute("id", "lon-indicator");
+		eSpanLon.innerText = "E/W"
+		eDivLon.appendChild(eSpanLon);
+		
+		var eSubmit = document.createElement("button");
+		eSubmit.type = "submit"
+		eSubmit.className = "submit-button";
+		eSubmit.innerText = "Go"
+		eForm.appendChild(eSubmit);
+		
+		setupAutocomplete(eInputLoc);
 	}
 }
--- peaksearch/this-scripts.js
+++ peaksearch/this-scripts.js
@@ -1,10 +1,9 @@
-window.onload = function() { main();};
+window.addEventListener("load", function() { main() } );
 
 function updateMapLocation(lat, lon, name)
 {
-   om_updateMapLocation(lat, lon, name)
-	 pf_setLocation(lat, lon, name)
-	 
+	om_updateMapLocation(lat, lon, name)
+	pf_setLocation(lat, lon, name)
 }
 
 function updateTimeUTC(lat, lon, name)
@@ -17,11 +16,13 @@ function main()
    layouts_init();
    initialize_map();
    add_map_point(-33.8688, 151.2093); 
-//   map_search_init()
 	 
 	pf_init();
 	pf_setPosition(0, 0, 0, 3, 270, 1, 30);
 	
 	updateMapLocation(-27.909281, 153.109104, 'Albert River')
 	updateTimeUTC(2026, 9, 26, 07, 30)
+	
+	createLocationLookupForm("locations-form-holder", "location-form", "Jump to Location", updateMapLocation )
  }
+ 
